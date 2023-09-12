c9c8d487f32ff086fed46ebdb881e0be8b7f091b
Uriel Ortega <urielortega2509@gmail.com>
2023-09-12 16:32:19 -0600

    Initial Commit

---
+++ CleanPad/CleanPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanPadApp.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
