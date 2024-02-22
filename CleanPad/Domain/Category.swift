daaa837872ab4de5489f265ff807a178de37740f
Uriel Ortega <urielortega2509@gmail.com>
2024-02-22 15:25:47 -0600

    Modified Category struct.

--- CleanPad/Domain/Category.swift
+++ CleanPad/Domain/Category.swift
@@ -9,18 +9,29 @@ import Foundation
 import SwiftUI
 
 /// Model defining the Note Category structure.
-struct Category: Codable, Identifiable, Equatable {
-    var id = UUID()
+struct Category: Codable, Identifiable, Equatable, Hashable {
+    var id: UUID
     var name: String
     var color: Color
 }
 
 extension Category {
+    // Static variable representing the default note category: General.
+    static var general = Category(
+        id: UUID(uuidString: "6EDDFE8C-0489-4455-9010-75D2D2A7BD37")!, // Static id for the permanent General Category.
+        name: "General",
+        color: .gray
+    )
+    
     static let example = Category(
+        id: UUID(),
         name: "Thoughts",
         color: .cyan
     )
     
-    // Static variable representing the default note category: General.
-    static var general = Category(name: "General", color: .gray)
+    static let emptySelection = Category(
+        id: UUID(), 
+        name: "No selection",
+        color: .pink
+    )
 }
