483389a594658f51c762c5483a6c6f4059446b19
Uriel Ortega <urielortega2509@gmail.com>
2024-03-18 16:07:53 -0600

    Created EditableCategoriesView.

---
+++ CleanPad/Presentation/EditableCategoriesView.swift
@@ -0,0 +1,19 @@
+//
+//  EditableCategoriesView.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 18/03/24.
+//
+
+import SwiftUI
+
+/// View that shows all user categories that can be edited by tapping one of them.
+struct EditableCategoriesView: View {
+    var body: some View {
+        Text("All Your Editable Categories")
+    }
+}
+
+#Preview {
+    EditableCategoriesView()
+}
