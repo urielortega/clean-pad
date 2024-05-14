0fd9ca20e7391227929456e2fa82d1ff2f93b612
Uriel Ortega <urielortega2509@gmail.com>
2024-05-13 19:07:18 -0600

    Added accessibilityEmptyListButton as an EmptyListView extension.

--- CleanPad/Presentation/Common/EmptyListView.swift
+++ CleanPad/Presentation/Common/EmptyListView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 /// View meant to be used when a list is empty, inviting the user to add an item.
 /// Can be personalized modifying its default parameters values.
 struct EmptyListView: View {
+    @Binding var showEditViewSheet: Bool
     var imageSystemName: String = "questionmark"
     var label: String = "No Items"
     var description: String = "Start adding items to your list."
@@ -40,8 +41,25 @@ struct EmptyListView: View {
     }
 }
 
+extension EmptyListView {
+    var accessibilityEmptyListButton: some View {
+        Button {
+            showEditViewSheet.toggle()
+        } label: {
+            EmptyListView(
+                showEditViewSheet: $showEditViewSheet,
+                imageSystemName: "note.text",
+                label: "This looks a little empty...",
+                description: Constants.emptyListPlaceholders.randomElement() ?? "Start writing...",
+                buttonLabel: "Create a note!"
+            ) {}
+        }
+        .accessibilityLabel("Empty list. Tap to create a note.")
+    }
+}
+
 struct EmptyListView_Previews: PreviewProvider {
     static var previews: some View {
-        EmptyListView() { }
+        EmptyListView(showEditViewSheet: .constant(false)) { }
     }
 }
---
+++ CleanPad/Presentation/Common/UnlockNotesView.swift
@@ -0,0 +1,18 @@
+//
+//  UnlockNotesView.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 13/05/24.
+//
+
+import SwiftUI
+
+struct UnlockNotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnlockNotesView()
+}
