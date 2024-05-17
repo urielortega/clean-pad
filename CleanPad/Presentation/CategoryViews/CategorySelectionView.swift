5ea5d7a705739f10c54d83448128401ba92f7f9b
Uriel Ortega <urielortega2509@gmail.com>
2024-05-17 13:54:22 -0600

    Renamed CategoryButtonLabel to CategoryButton, and used in CategorySelectionView.

--- CleanPad/Presentation/CategoryViews/CategorySelectionView.swift
+++ CleanPad/Presentation/CategoryViews/CategorySelectionView.swift
@@ -34,34 +34,52 @@ struct CategorySelectionView: View {
             ScrollView {
                 LazyVStack {
                     ForEach(viewModel.categories) { category in
-                        if isEditModeActive {
-                            Button("Editable Category Button") { sheetsViewModel.showCategoryEditViewSheet.toggle() }
-                        } else {
-                            SelectableCategoryButton(
-                                category: category,
+                        CategoryButton(
+                            viewModel: viewModel,
+                            sheetsViewModel: sheetsViewModel,
+                            category: category,
+                            role: isEditModeActive ? .edition : .selection
+                        ) {
+                            // TODO: Refactor and move to VM:
+                            if isEditModeActive {
+                                sheetsViewModel.showCategoryEditViewSheet.toggle()
+                            } else {
+                                withAnimation(.bouncy) {
+                                    viewModel.changeSelectedCategory(with: category)
+                                }
+                                HapticManager.instance.impact(style: .soft)
+                                
+                                dismiss()
+                                viewModel.customTabBarGlow()
+                            }
+                        }
+                        
+                        if !isEditModeActive {
+                            Divider()
+                            
+                            CategoryButton(
                                 viewModel: viewModel,
-                                sheetsViewModel: sheetsViewModel
-                            )
+                                sheetsViewModel: sheetsViewModel,
+                                category: .noSelection,
+                                role: isEditModeActive ? .edition : .selection
+                            ) {
+                                // TODO: Refactor and move to VM:
+                                withAnimation(.bouncy) {
+                                    viewModel.changeSelectedCategory(with: .noSelection)
+                                }
+                                HapticManager.instance.impact(style: .soft)
+                                
+                                dismiss()
+                                viewModel.customTabBarGlow()
+                            }
                         }
                     }
-                    
-                    if !isEditModeActive {
-                        Divider()
-                            .padding(.vertical)
-                        
-                        SelectableCategoryButton(
-                            category: .noSelection,
-                            viewModel: viewModel,
-                            sheetsViewModel: sheetsViewModel
-                        )
-                    }
+                    .padding()
                 }
-                .padding()
             }
             
             editCategoriesButton
                 .padding(.vertical)
-                .padding(.bottom)
         }
         .padding(.top)
         .presentationDetents([.fraction(0.5), .large])
@@ -81,73 +99,8 @@ struct CategorySelectionView: View {
     }
 }
 
-/// A selectable View that shows a category name and color.
-struct SelectableCategoryButton: View {
-    var category: Category
-    @ObservedObject var viewModel: NotesListViewModel
-    @ObservedObject var sheetsViewModel: SheetsViewModel
-    @Environment(\.dismiss) var dismiss
-        
-    var strokeColorGradient: AnyGradient {
-        viewModel.selectedCategory == category ? category.color.gradient : Color.gray.gradient
-    }
-    
-    var body: some View {
-        HStack {
-            Label("\(category.name)", systemImage: "note.text")
-                .imageScale(.medium)
-                .foregroundStyle(category.color.gradient)
-            
-            Spacer()
-            
-            if viewModel.selectedCategory == category {
-                Image(systemName: "checkmark")
-                    .foregroundStyle(category.color)
-                    .bold()
-            }
-        }
-        .padding()
-        .background(.regularMaterial)
-        .clipShape(.rect(cornerRadius: 10))
-        .overlay {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(
-                    strokeColorGradient.opacity(
-                        viewModel.selectedCategory == category ? 0.5 : 0.1
-                    ),
-                    lineWidth: 3
-                )
-        }
-        .shadow(color: .gridLabelShadow, radius: 2, x: 0, y: 6)
-        .onTapGesture {
-            withAnimation(.bouncy) { viewModel.changeSelectedCategory(with: category) }
-            
-            HapticManager.instance.impact(style: .soft)
-            dismiss()
-            
-            // CustomTabBar Glow:
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation(.easeInOut(duration: 1)) {
-                    viewModel.isCustomTabBarGlowing.toggle()
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    withAnimation(.easeInOut(duration: 1)) {
-                        viewModel.isCustomTabBarGlowing.toggle()
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-enum CategoryButtonRole {
-    case selection
-    case edition
-}
-
-/// View to be used as label for buttons that adapt for Category Edition and Selection modes.
-struct CategoryButtonLabel: View {
+/// View for a Button that adapts according to Category Edition and Selection modes.
+struct CategoryButton: View {
     @ObservedObject var viewModel: NotesListViewModel
     @ObservedObject var sheetsViewModel: SheetsViewModel
     @Environment(\.dismiss) var dismiss
@@ -160,6 +113,11 @@ struct CategoryButtonLabel: View {
         viewModel.selectedCategory == category ? category.color.gradient : Color.gray.gradient
     }
     
+    enum CategoryButtonRole {
+        case selection
+        case edition
+    }
+
     var body: some View {
         HStack {
             Label("\(category.name)", systemImage: "note.text")
@@ -199,17 +157,10 @@ struct CategoryButtonLabel: View {
             }
         }
         .shadow(color: .gridLabelShadow, radius: 2, x: 0, y: 6)
-        .onTapGesture {
-            buttonActions()
-        }
+        .onTapGesture { buttonActions() }
     }
 }
 
 #Preview("CategorySelectionView Sheet") {
     CategorySelectionView(viewModel: NotesListViewModel(), sheetsViewModel: SheetsViewModel())
-}
-
-#Preview("CategoryFilteringLabel") {
-    SelectableCategoryButton(category: .example, viewModel: NotesListViewModel(), sheetsViewModel: SheetsViewModel())
-        .padding()
 }
