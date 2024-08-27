096abcac8b8338909f49d3a48823cbcae69a8b28
Uriel Ortega <urielortega2509@gmail.com>
2024-08-27 16:52:33 -0600

    Created NoteCategorySelectionView file.

---
+++ CleanPad/Presentation/CategoryViews/NoteCategorySelectionView.swift
@@ -0,0 +1,15 @@
+//
+//  NoteCategorySelectionView.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 27/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct NoteCategorySelectionView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
