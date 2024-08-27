e8bc17cb5d0cfefef639256dc9a2271b36aef3ab
Uriel Ortega <urielortega2509@gmail.com>
2024-08-27 17:10:23 -0600

    Created NoteCategorySelectionView extension for secondary views.

--- CleanPad/Presentation/CategoryViews/NoteCategorySelectionView.swift
+++ CleanPad/Presentation/CategoryViews/NoteCategorySelectionView.swift
@@ -16,17 +16,35 @@ struct NoteCategorySelectionView: View {
     
     var body: some View {
         VStack {
-            // dismissTopView
+            dismissTopView
             
             CustomTopTitle(text: "Assign a Category")
                 .padding(.horizontal)
             
-            // categoriesGridView
+            categoriesGridView
         }
         .padding(.top)
         .presentationDragIndicator(.visible)
         .presentationBackground(.regularMaterial)
         .presentationCornerRadius(Constants.roundedRectCornerRadius)
         .presentationDetents([.fraction(0.6)])
+    }
+}
+
+// MARK: - Extension to group secondary views in NoteCategorySelectionView.
+extension NoteCategorySelectionView {
+    var dismissTopView: some View {
+        HStack {
+            Spacer()
+            DismissViewButton()
+        }
+        .padding([.horizontal, .bottom])
+    }
+    
+    var categoriesGridView: some View {
+        // TODO: Replace with a grid that shows all user categories:
+        GeometryReader { _ in
+            EmptyView()
+        }
     }
 }
