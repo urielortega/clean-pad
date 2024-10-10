d6f68c32b0178f85b304c445567e90dab0215d0c
Uriel Ortega <urielortega2509@gmail.com>
2024-10-09 19:02:23 -0600

    Created NoResultsView.

---
+++ CleanPad/Presentation/Common/NoResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoResultsView.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 09/10/24.
+// Inspired by ContentUnavailableView (iOS 17+)
+
+import SwiftUI
+
+struct NoResultsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    NoResultsView()
+}
