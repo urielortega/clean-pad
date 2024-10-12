d580054133ab8d58cace6f15be857f84e7dada7d
Uriel Ortega <urielortega2509@gmail.com>
2024-10-12 14:01:39 -0600

    Created initial layout for WhatsNewView.

--- CleanPad/Presentation/AboutViews/WhatsNewView.swift
+++ CleanPad/Presentation/AboutViews/WhatsNewView.swift
@@ -5,13 +5,56 @@
 //  Created by Uriel Ortega on 12/10/24.
 //
 
+import ColorfulX
 import SwiftUI
 
 struct WhatsNewView: View {
+    @Environment(\.dismiss) var dismiss
+    @State var gradientColors: [Color] = [.white, .cleanPadIconBackground, .white]
+    @State var gradientSpeed: Double = 0.4
+    
     var body: some View {
-        EmptyView()
+        ZStack(alignment: .center) {
+            ColorfulView(color: $gradientColors, speed: $gradientSpeed)
+                .opacity(0.9)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                Spacer()
+                headerView
+                detailsView
+                Spacer()
+                dismissButtonView
+            }
+        }
+        .presentationCornerRadius(Constants.roundedRectCornerRadius)
     }
 }
+
+// MARK: - Extension to group secondary views in WelcomeView.
+extension WhatsNewView {
+    /// Header view containing the "What's New" Text.
+    var headerView: some View {
+        EmptyView()
+    }
+    
+    /// Detailed view explaining the changes in the new version of the app.
+    var detailsView: some View {
+        EmptyView()
+    }
+    
+    /// Button for dismissing the What's New screen.
+    var dismissButtonView: some View {
+        Button {
+            HapticManager.instance.impact(style: .soft)
+            dismiss()
+        } label: {
+            MaterialButtonLabel(labelText: "Great!")
+        }
+        .padding()
+    }
+}
+
 
 #Preview {
     WhatsNewView()
