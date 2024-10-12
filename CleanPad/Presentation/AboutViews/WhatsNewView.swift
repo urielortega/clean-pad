366216ff238efe1fb549e70f0470ca1939226d19
Uriel Ortega <urielortega2509@gmail.com>
2024-10-12 13:48:01 -0600

    Created WhatsNewView.swift.

---
+++ CleanPad/Presentation/AboutViews/WhatsNewView.swift
@@ -0,0 +1,18 @@
+//
+//  WhatsNewView.swift
+//  CleanPad
+//
+//  Created by Uriel Ortega on 12/10/24.
+//
+
+import SwiftUI
+
+struct WhatsNewView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    WhatsNewView()
+}
