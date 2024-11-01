8727acde5d2a44b0a027fc63bb2abef87f57d9af
Uriel Ortega <urielortega2509@gmail.com>
2024-10-31 22:00:00 -0600

    Added documentation in HapticManager.swift file.

--- CleanPad/Helpers/HapticManager.swift
+++ CleanPad/Helpers/HapticManager.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 
+/// A custom singleton class to manage haptic feedback throughout the app.
+/// Provides easy-to-use methods for generating notification and impact haptic feedback.
 class HapticManager {
+    /// Shared instance of `HapticManager` to enable global access to haptic feedback methods.
     static let instance = HapticManager()
     
+    /// Triggers a notification-style haptic feedback.
+    /// Use this method to give the user tactile feedback in response to significant actions or changes.
     func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(type)
     }
     
+    /// Triggers an impact-style haptic feedback.
+    /// Use this method to give the user a subtle tactile response, ideal for smaller, less significant actions.
     func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
         let generator = UIImpactFeedbackGenerator(style: style)
         generator.impactOccurred()
