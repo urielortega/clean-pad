b47401fc8f60fa0efc805191c9221a8b9c10de6c
Uriel Ortega <urielortega2509@gmail.com>
2025-01-09 18:45:56 -0600

    Created BlurWhenAppNotActive ViewModifier.

--- CleanPad/Presentation/Common/ViewModifiers/BlurViewModifiers.swift
+++ CleanPad/Presentation/Common/ViewModifiers/BlurViewModifiers.swift
@@ -7,3 +7,23 @@
 
 import SwiftUI
 
+/// ViewModifier to blur a view when the app is inactive and the `isBlurActive` flag is set to `true`.
+struct BlurWhenAppNotActive: ViewModifier {
+    /// A boolean flag that determines whether the blur effect should be applied.
+    let isBlurActive: Bool
+    
+    /// Property to blur locked notes when phase changes.
+    @Environment(\.scenePhase) private var scenePhase
+ 
+    func body(content: Content) -> some View {
+        content
+            .blur(radius: (scenePhase != .active) ? 10 : 0)
+            .animation(.default, value: scenePhase)
+    }
+}
+
+extension View {
+    public func blurWhenAppNotActive(isBlurActive: Bool) -> some View {
+        modifier(BlurWhenAppNotActive(isBlurActive: isBlurActive))
+    }
+}
