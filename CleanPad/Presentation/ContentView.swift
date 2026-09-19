d35d6282b8a8bd12832bc2f12aa28c20cbfb66e6
Uriel Ortega <urielortega2509@gmail.com>
2026-09-18 19:49:34 -0600

    feat(ui): apply conditional scrollEdgeEffectStyle

--- CleanPad/Presentation/ContentView.swift
+++ CleanPad/Presentation/ContentView.swift
@@ -74,6 +74,7 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .progressiveTopScrollEdgeEffect()
                 }
             }
         }
